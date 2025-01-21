1cb81b58615c30e80c4de0cc65705b06e7f2e6ca
vitorhugomarcal <vitorhugomarcal@gmail.com>
2025-01-21 14:08:44 -0300

    token

--- src/http/server.ts
+++ src/http/server.ts
@@ -21,7 +21,7 @@ const app = new Elysia()
       credentials: true,
       allowedHeaders: ["content-type"],
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
-      origin: true,
+      origin: "https://orbizy.app",
       // origin: (request): boolean => {
       //   const origin = request.headers.get("origin")
 
