33caa2f496c5b182e125af5245616c349529182a
vitorhugomarcal <vitorhugomarcal@gmail.com>
2025-01-22 15:29:04 -0300

    signout

--- src/http/routes/logout.ts
+++ src/http/routes/logout.ts
@@ -3,11 +3,4 @@ import { removeCookie } from "../../utils/cookie"
 
 export const logoutRoute = new Elysia().get("/signout", async ({ cookie }) => {
   removeCookie(cookie.auth)
-
-  return new Response(null, {
-    status: 302,
-    headers: {
-      Location: "/sign-in",
-    },
-  })
 })
