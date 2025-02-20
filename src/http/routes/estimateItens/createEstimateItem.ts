b8eb1dd71e0e0bc2fe42b58e76a74b1d7dcf9b60
vitorhugomarcal <vitorhugomarcal@gmail.com>
2025-02-20 13:51:03 -0300

    estimate

--- src/http/routes/estimateItens/createEstimateItem.ts
+++ src/http/routes/estimateItens/createEstimateItem.ts
@@ -55,7 +55,6 @@ export const createEstimateItem = new Elysia().post(
   {
     body: t.Object({
       name: t.String(),
-      description: t.Nullable(t.String()),
       quantity: t.Number(),
       price: t.Number(),
       unit: t.String(),
@@ -71,7 +70,6 @@ export const createEstimateItem = new Elysia().post(
           item: t.Object({
             id: t.String(),
             name: t.String(),
-            description: t.Nullable(t.String()),
             quantity: t.Number(),
             price: t.Number(),
             unit: t.String(),
