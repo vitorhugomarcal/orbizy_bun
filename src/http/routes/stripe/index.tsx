b049343bea48235bd081deb66b493cb3e948cefb
vitorhugomarcal <vitorhugomarcal@gmail.com>
2025-03-18 15:34:28 -0300

    stripe account

--- src/http/routes/stripe/index.tsx
+++ src/http/routes/stripe/index.tsx
@@ -1,6 +1,7 @@
 import { Elysia } from "elysia"
+import { createInvoice } from "./create"
 import { registerCompany } from "./register"
 
-const stripeRoutes = new Elysia().use(registerCompany)
+const stripeRoutes = new Elysia().use(registerCompany).use(createInvoice)
 
 export default stripeRoutes
