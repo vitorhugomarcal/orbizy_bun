d9adc934f25d44b74bd771d05e362efd84849251
vitorhugomarcal <vitorhugomarcal@gmail.com>
2025-03-19 10:21:46 -0300

    stripe account

--- src/http/routes/stripe/index.tsx
+++ src/http/routes/stripe/index.tsx
@@ -1,11 +1,13 @@
 import { Elysia } from "elysia"
 import { createInvoice } from "./create"
+import { redirect } from "./redirect"
 import { registerCompany } from "./register"
 import { removeStripe } from "./remove"
 
 const stripeRoutes = new Elysia()
-  .use(registerCompany)
-  .use(createInvoice)
+  .use(redirect)
   .use(removeStripe)
+  .use(createInvoice)
+  .use(registerCompany)
 
 export default stripeRoutes
---
+++ src/http/routes/stripe/redirect.ts
@@ -0,0 +1,28 @@
+import Elysia, { t } from "elysia"
+
+export const redirect = new Elysia().get(
+  "/redirect-to-app",
+  async ({ set }) => {
+    const appScheme = "vhminvoice://stripe-auth"
+    set.headers["Location"] = appScheme
+    set.status = 302
+
+    return { message: "Redirecting to app" }
+  },
+  {
+    response: {
+      302: t.Object(
+        {
+          message: t.String(),
+        },
+        {
+          description: "Redirect to app",
+        }
+      ),
+    },
+    detail: {
+      description: "Redirect to app after Stripe onboarding",
+      tags: ["Stripe"],
+    },
+  }
+)
