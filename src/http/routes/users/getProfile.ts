9e0ae35b0e68b656e8c0b9185c000179843122eb
vitorhugomarcal <vitorhugomarcal@gmail.com>
2025-03-21 12:14:58 -0300

    user scheme

--- src/http/routes/users/getProfile.ts
+++ src/http/routes/users/getProfile.ts
@@ -9,9 +9,7 @@ export const getProfile = new Elysia().get(
     if (!user) {
       throw new AuthError("Unauthorized", "UNAUTHORIZED", 401)
     }
-
     console.log("user", user)
-
     return {
       message: "Profile retrieved successfully",
       user,
@@ -43,19 +41,7 @@ export const getProfile = new Elysia().get(
                 phone: t.String(),
                 stripeAccountId: t.Nullable(t.String()),
                 owner_id: t.String(),
-                address: t.Object({
-                  id: t.String(),
-                  country: t.String(),
-                  city: t.String(),
-                  state: t.String(),
-                  postal_code: t.String(),
-                  cep: t.Nullable(t.String()),
-                  street: t.Nullable(t.String()),
-                  number: t.Nullable(t.String()),
-                  neighborhood: t.Nullable(t.String()),
-                  street_address: t.Nullable(t.String()),
-                  unit_number: t.Nullable(t.String()),
-                }),
+                address_id: t.String(),
               })
             ),
           }),
