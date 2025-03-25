477b6941898bf06bd451e975525b268aa3b5c25d
vitorhugomarcal <vitorhugomarcal@gmail.com>
2025-03-25 13:12:06 -0300

    mp

--- src/http/routes/mercadoPago/index.tsx
+++ src/http/routes/mercadoPago/index.tsx
@@ -1,9 +1,11 @@
 import { Elysia } from "elysia"
 import { createPreferences } from "./createPreferences"
 import { exchangeToken } from "./exchange"
+import { redirect } from "./redirect"
 import { refreshToken } from "./refreshToken"
 
 const mercadoPagoRoutes = new Elysia()
+  .use(redirect)
   .use(refreshToken)
   .use(exchangeToken)
   .use(createPreferences)
