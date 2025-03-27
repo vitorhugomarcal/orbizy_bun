7870f90af2c0bebfcb81dd4609fb1ef2fb5f7353
vitorhugomarcal <vitorhugomarcal@gmail.com>
2025-03-27 10:16:04 -0300

    payment

--- prisma/migrations/20250325132109_mercado_pago/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "mp_at" TEXT,
-ADD COLUMN     "mp_id" TEXT,
-ADD COLUMN     "mp_rt" TEXT;
--- prisma/migrations/20250327124451_pix/migration.sql
+++ prisma/migrations/20250327124451_pix/migration.sql
@@ -1,15 +1,2 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `mp_at` on the `users` table. All the data in the column will be lost.
-  - You are about to drop the column `mp_id` on the `users` table. All the data in the column will be lost.
-  - You are about to drop the column `mp_rt` on the `users` table. All the data in the column will be lost.
-
-*/
 -- AlterTable
 ALTER TABLE "payments_mode_custom" ADD COLUMN     "code" TEXT;
-
--- AlterTable
-ALTER TABLE "users" DROP COLUMN "mp_at",
-DROP COLUMN "mp_id",
-DROP COLUMN "mp_rt";
